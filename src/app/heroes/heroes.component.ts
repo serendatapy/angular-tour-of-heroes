b2857c9e534eacc5dbad6d7b2b7a1e658f179a28
serendatapy <arvaldivia@gmail.com>
2020-10-21 17:16:08 +0200

    Added styling and heroes list to hero component

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+/*@Component is a decorator function
+that specifies the Angular metadata
+for the component. */
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+/*Always export the component class
+so you can import it elsewhere */
+export class HeroesComponent implements OnInit {
+  // all variables here are accessible from template
+  heroes = HEROES;
+
+  hero:Hero = {
+  id: 1,
+  name: 'Windstorm',
+  };
+
+  constructor() { }
+
+  /*The ngOnInit() is a lifecycle hook.
+  Angular calls ngOnInit() shortly after
+  creating a component. It's a good place
+  to put initialization logic. */
+  ngOnInit(): void {
+  }
+
+}
